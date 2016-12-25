6fdde2a7cd79f1e435a75b1af248201cf530a531
John Ruble <johnruble@gmail.com>
2016-12-24 21:21:36 -0500

    use async/await

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(){
-    return Ember.$.getJSON('/api/location')
-      .then((loc) => Ember.$.getJSON(`/api/weather?lat=${loc.lat}&long=${loc.long}`))
-      .then((weather) => Ember.$.getJSON(`/api/advice?weather=${weather.weather}`));
+  async model(){
+    const loc = await Ember.$.getJSON('/api/location');
+    const weather = await Ember.$.getJSON(`/api/weather?lat=${loc.lat}&long=${loc.long}`);
+    return Ember.$.getJSON(`/api/advice?weather=${weather.weather}`);
   },
 });
